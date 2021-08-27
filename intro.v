1ad4bbc8dd5c8caf251786c421f68a30ea5fa04c
Vikram Belthur <46857503+VikBelt@users.noreply.github.com>
2021-08-27 18:55:29 -0400

    Add files via upload

---
+++ intro.v
@@ -0,0 +1,9 @@
+module intro (A,B);
+
+    //define input and output
+    input A;
+    output B;
+
+    assign B = A;
+    
+endmodule
---
+++ intro_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns/1ns
+`include "intro.v"
+
+module intro_tb();
+
+reg A;
+wire B;
+
+intro DUT(A, B);
+
+initial begin
+
+    $dumpfile("intro_tb.vcd");
+    $dumpvars(9, intro_tb);
+
+    A = 0;
+
+    #20
+    A = 1;
+
+    #20
+    A = 0;
+
+    #20
+    $display("Test Complete - 0 Errors");
+
+end
+endmodule
